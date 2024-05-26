e829e68f321545f54977481e2f86ec6b64c49e68
der_frühling <works-on-my-machine@der-fruhling.mozmail.com>
2024-05-26 02:42:20 -0700

    important changes

--- common/src/main/java/net/derfruhling/minecraft/markit/Markit.java
+++ common/src/main/java/net/derfruhling/minecraft/markit/Markit.java
@@ -1,9 +1,13 @@
 package net.derfruhling.minecraft.markit;
 
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
 public class Markit {
 	public static final String MOD_ID = "markit";
+	private static final Logger LOG = LogUtils.getLogger();
 
 	public static void init() {
-		
+		LOG.info("marking it");
 	}
 }
