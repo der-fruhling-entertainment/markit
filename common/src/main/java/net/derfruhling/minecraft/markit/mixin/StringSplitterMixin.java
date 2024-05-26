3f5b790f4f5d413d3800bb1fbd84ccdc1dbd1c5d
der_frühling <works-on-my-machine@der-fruhling.mozmail.com>
2024-05-26 02:33:13 -0700

    Remove StringSplitter mixin

--- common/src/main/java/net/derfruhling/minecraft/markit/mixin/StringSplitterMixin.java
+++
@@ -1,9 +0,0 @@
-package net.derfruhling.minecraft.markit.mixin;
-
-import net.minecraft.client.StringSplitter;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(StringSplitter.class)
-public class StringSplitterMixin {
-
-}
