fefacdad3e7b07d085864c465a83023359408775
der_frühling <works-on-my-machine@der-fruhling.mozmail.com>
2024-06-02 19:01:19 -0700

    #1 Don't apply markdown formatting for commands

--- common/src/main/java/net/derfruhling/minecraft/markit/mixin/CommandSuggestionsMixin.java
+++ common/src/main/java/net/derfruhling/minecraft/markit/mixin/CommandSuggestionsMixin.java
@@ -1,8 +1,11 @@
 package net.derfruhling.minecraft.markit.mixin;
 
-import net.derfruhling.minecraft.markit.Keeper;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.brigadier.ParseResults;
 import net.derfruhling.minecraft.markit.MarkdownHam;
 import net.minecraft.client.gui.components.CommandSuggestions;
+import net.minecraft.commands.SharedSuggestionProvider;
 import net.minecraft.network.chat.Style;
 import net.minecraft.util.FormattedCharSequence;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,5 +17,10 @@ public class CommandSuggestionsMixin {
     @Redirect(method = "formatChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/FormattedCharSequence;forward(Ljava/lang/String;Lnet/minecraft/network/chat/Style;)Lnet/minecraft/util/FormattedCharSequence;"))
     public FormattedCharSequence formatChat(String string, Style emptyStyle) {
         return MarkdownHam.createEditorCharSequence(string, emptyStyle);
+    }
+
+    @WrapOperation(method = "formatChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/CommandSuggestions;formatText(Lcom/mojang/brigadier/ParseResults;Ljava/lang/String;I)Lnet/minecraft/util/FormattedCharSequence;"))
+    public FormattedCharSequence disableMarkdownForCommands(ParseResults<SharedSuggestionProvider> parseResults, String string, int i, Operation<FormattedCharSequence> original) {
+        return MarkdownHam.disabled(() -> original.call(parseResults, string, i));
     }
 }
